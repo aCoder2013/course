1037cc75a1be303fbbb4666b6b62aa386eb5de50
王松松 <songsong@taovip.com>
2017-02-11 15:47:24 +0800

    增加对不需要权限过滤的路径的支持

--- course-api-gateway/src/main/java/com/song/config/SecurityConfig.java
+++ course-api-gateway/src/main/java/com/song/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.song.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -9,11 +11,17 @@ import org.springframework.security.crypto.password.PasswordEncoder;
  * Created by song on 2017/2/5.
  */
 @Configuration
-public class SecurityConfig  {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    @Override
+    public void init(WebSecurity web) throws Exception {
+        super.init(web);
+        web.ignoring()
+                .antMatchers("/");
+    }
 }
