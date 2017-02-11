89d8e4642b9fa77baf50f3b348c708a75d555c83
王松松 <songsong@taovip.com>
2017-02-11 15:49:13 +0800

    不过滤注册、登录路径

--- course-api-gateway/src/main/java/com/song/config/SecurityConfig.java
+++ course-api-gateway/src/main/java/com/song/config/SecurityConfig.java
@@ -22,6 +22,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public void init(WebSecurity web) throws Exception {
         super.init(web);
         web.ignoring()
-                .antMatchers("/");
+                .antMatchers("/","/users/register","/users/login");
     }
 }
